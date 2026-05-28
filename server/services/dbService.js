71694f6e49206b01db6850b150c6d49535d743d8
ert545855-cloud <ert545855@gmail.com>
2026-05-29 01:50:35 +0300

    Update dbService.js

--- server/services/dbService.js
+++ server/services/dbService.js
@@ -7,7 +7,7 @@ const logger = require('../utils/logger');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },  // Her ortamda açık
+  ssl: { rejectUnauthorized: false },
   max: 10,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 5000,
